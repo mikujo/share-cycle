2ee09e453a561b9a5d07e5a3286cdbb817ceadd2
mikujo <69742174+mikujo@users.noreply.github.com>
2020-09-29 15:06:44 +0900

    ビューの設定

--- app/controllers/cycles_controller.rb
+++ app/controllers/cycles_controller.rb
@@ -2,4 +2,23 @@ class CyclesController < ApplicationController
   def index
     @cycle = Cycle.all
   end
+
+  def new
+    @cycle = Cycle.new
+  end
+
+  def create
+    @cycle = Cycle.new(cycle_params)
+    if @cycle.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cycle_params
+    params.require(:cycle).permit(:image, :name, :detail, :price)
+  end
 end
