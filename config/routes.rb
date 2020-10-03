b5033ed7ebfd737acbff1cc0f75316391618f6ff
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-03 20:28:43 +0900

    コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'cycles/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "cycles#index"
   resources :users, only: [:edit, :update]
   resources :cycles
