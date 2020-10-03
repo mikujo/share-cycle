09c6e1f1c8fe97d4a63714453ee415a955b7fadb
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-03 19:36:46 +0900

    editページへのリンクの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'cycles/index'
   devise_for :users
   root to: "cycles#index"
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update]
   resources :cycles
 end
