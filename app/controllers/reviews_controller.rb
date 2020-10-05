a84cbdebb7657ffe757969c3c5fdddd6cb983296
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-05 16:31:35 +0900

    コントローラーの設定

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  before_action :set_cycle
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def set_cycle
+    @cycle = Cycle.find(params[:cycle_id])
+  end
+
+    private
+
+    def review_params
+      params.require(:review).permit(:title, :detail, :area_id, :rate, :user, :cycle)
+    end
+  
+end
