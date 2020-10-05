5c3a9a0be017e90e340f5ad9318526096e531e13
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-05 15:21:28 +0900

    ゲストユーザー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    post 'user/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: "cycles#index"
   resources :users, only: [:edit, :update]
   resources :cycles
