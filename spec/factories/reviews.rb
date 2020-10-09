e438b6217757544aeb8a55131ffa283c20ea3659
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-09 15:15:12 +0900

    テストコードの実行

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :review do
+    title { 'レビューのタイトル' }
+    detail { 'レビューの内容です。' }
+    area_id { 3 }
+    rate { 5 }
+    association :user
+    association :cycle
   end
 end
