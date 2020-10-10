1131be62c6cfa3d730ceb802abb6607e3f9585c6
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-10 16:30:14 +0900

    コメントの保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to  cycle_review_path(comment.review.id)
+    @cycle = Cycle.find(params[:cycle_id])
+    @review = Review.find(params[:review_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to cycle_review_path(cycle_id: @cycle.id, id: @review.id)
+    else
+      redirect_to cycle_review_path(cycle_id: @cycle.id, id: @review.id)
+    end
   end
 
   def destroy
@@ -12,5 +18,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, review_id: params[:review_id])
   end
-
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,8 @@ class ReviewsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @review.comments.includes(:user)
   end
 
   def edit
