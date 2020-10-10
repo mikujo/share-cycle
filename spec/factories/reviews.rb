128ac7b1f35af120688737813112eb3ec48d20fe
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-10 18:07:45 +0900

    テストコードの実行

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :review do
-    title { 'レビューのタイトル' }
-    detail { 'レビューの内容です。' }
+    title   { 'レビューのタイトル' }
+    detail  { 'レビューの内容です。' }
     area_id { 3 }
-    rate { 5 }
+    rate    { 5 }
     association :user
     association :cycle
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメント機能' do
+    before do 
+      @comment = FactoryBot.build(:comment)
+    end
+
+    context '新規投稿がうまくいく時' do
+      it '全ての値が正しく入力されていれば保存できること' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context '新規投稿がうまくいかない時' do
+      it 'textが空だと保存できないこと' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('User must exist')
+      end
+      it 'reviewが紐付いていないと保存できないこと' do
+        @comment.review = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Review must exist')
+      end
+    end
+  end
 end
