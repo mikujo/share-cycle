6a388eae9d42d9523b724cc919380444af94b3cd
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-10 18:41:42 +0900

    削除後の遷移

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_cycle, only: [:create, :destroy]
+  before_action :set_review, only: [:create, :destroy]
+  before_action :set_comment, only: [:destroy, :move_to_index]
+  before_action :move_to_index, only: [:destroy]
+
   def create
-    @cycle = Cycle.find(params[:cycle_id])
-    @review = Review.find(params[:review_id])
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to cycle_review_path(cycle_id: @cycle.id, id: @review.id)
@@ -11,6 +15,28 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    if @comment.destroy
+      redirect_to cycle_review_path(cycle_id: @cycle.id, id: @review.id)
+    else
+      redirect_to cycle_review_path(cycle_id: @cycle.id, id: @review.id)
+    end
+  end
+
+  def set_cycle
+    @cycle = Cycle.find(params[:cycle_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:review_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @comment.user_id
   end
 
   private
