f3da09d2e0510a622a9edd1fee5266f9978b1e3d
mikujo <69742174+mikujo@users.noreply.github.com>
2020-10-20 21:59:41 +0900

    リファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname { Faker::Name.last_name }
     email    { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password { password}
+    password { password }
     password_confirmation {password}
   end
 end
